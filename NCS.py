839cb2a5fe3fc2d6b8e277b0571c4cd768aa2227
Young Jin Kim <kimyoungjin06@gmail.com>
2017-09-30 20:39:57 +0900

    add dtype

--- NCS.py
+++ NCS.py
@@ -1,9 +1,10 @@
-def NNCS(url, sort = 'favorite', pageSize = 100, page = 1, scrapeAll = True):
+def NNCS(url, sort = 'favorite', pageSize = 100, page = 1, scrapeAll = True, dtype = 'list'):
     '''
         pageSize = 100
         page = 1
         sort = "favorite", "reply", "old", "new"
         scrapeAll = True, False
+        dtype = "list", "json"
     ''' 
     params = []
     for i in url.split('?')[1].split('&'):
@@ -18,8 +19,12 @@ def NNCS(url, sort = 'favorite', pageSize = 100, page = 1, scrapeAll = True):
     }
     res = rq.get(url, headers = header)
     commentSize = json.loads('('.join(res.text.split('(')[1:])[:-2])['result']['count']['comment']
-    comments = [x['contents'] for x in json.loads('('.join(res.text.split('(')[1:])[:-2])['result']['commentList']]
     
+    if dtype == 'list':
+        comments = [x['contents'] for x in json.loads('('.join(res.text.split('(')[1:])[:-2])['result']['commentList']]
+    else :
+        comments = json.loads('('.join(res.text.split('(')[1:])[:-2])['result']['commentList']
+        
     if scrapeAll == True:
         for page in range(2,int(commentSize/100)+2):
             url = "https://apis.naver.com/commentBox/cbox/web_naver_list_jsonp.json?ticket=news&templateId=view_politics&pool=cbox5&lang=ko&country=KR&objectId=news"+\
@@ -27,7 +32,10 @@ def NNCS(url, sort = 'favorite', pageSize = 100, page = 1, scrapeAll = True):
                 str(pageSize)+ "&indexSize=10&groupId=&page="+ str(page)+ "&initialize=true&useAltSort=true&replyPageSize=30&moveTo=&sort="+\
                 sort
             res = rq.get(url, headers = header)
-            comments = comments + [x['contents'] for x in json.loads('('.join(res.text.split('(')[1:])[:-2])['result']['commentList']]
+            if dtype == 'list':
+                comments = comments + [x['contents'] for x in json.loads('('.join(res.text.split('(')[1:])[:-2])['result']['commentList']]
+            else :
+                comments = comments + json.loads('('.join(res.text.split('(')[1:])[:-2])['result']['commentList']
             print("%.2f" % (len(comments)/commentSize),"percent complete...")
             time.sleep(2)
     
